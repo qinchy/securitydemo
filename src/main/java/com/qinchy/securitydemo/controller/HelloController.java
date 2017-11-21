446337a1d9fefe1a43055bfdccd7bfa6aaff7a89
qinchy <qinchy@msn.com>
2017-11-21 17:24:14 +0800

    添加程序文件

--- src/main/java/com/qinchy/securitydemo/controller/HelloController.java
+++ src/main/java/com/qinchy/securitydemo/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloController {
 
-    @RequestMapping(path = "/index")
+    @RequestMapping(path = {"/index","/"})
     public String index(Model model){
         Msg msg = new Msg("测试标题","测试内容","额外信息");
         model.addAttribute("msg",msg);
